6b27d8c6bdfc6a04976d073858b0a13e3b09e2e9
harshithag <harshithags@gmail.com>
2022-04-15 14:42:02 +0530

    First Commit +1 Added a new line

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def new_func(a,b):
+    return a + b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Harshitha')
     print('How are you?')
+
+    print(new_func(2,4))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
