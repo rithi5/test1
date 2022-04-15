2aabd87e02ff8be5a2ae13ebdedf85b7e9bee2f9
rithi5 <103739453+rithi5@users.noreply.github.com>
2022-04-15 15:00:05 +0530

    Update main.py
    Changed + to -

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def new_func(a,b):
-    return a + b
+    return a - b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Harshitha')
     print('How are you?')
 
-    print(new_func(2,4))
+    print(new_func(8, 2))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
